5a0976da85fa1b40d64f95f862eda7c36402c76a
DEF4LT-303 <ryanrafi72@gmail.com>
2022-09-27 16:36:24 +0600

    cart button added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import banner from "../../assets/banner.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = props => {
+  return( <Fragment>
+    <header className={classes.header}>
+      <h1>Game Shop</h1>
+      <HeaderCartButton />
+    </header>
+
+    <div className={classes['main-image']}>
+      <img src={banner} alt="banner" />
+    </div>
+  </Fragment>);
+};
+
+export default Header;
