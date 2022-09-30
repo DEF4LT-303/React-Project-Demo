fdc4b000a0898632d2fa54aa55fa5d4d0eaa3974
DEF4LT-303 <ryanrafi72@gmail.com>
2022-09-30 14:42:06 +0600

    Title and icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import { Helmet } from 'react-helmet';
 import Cart from './components/Cart/Cart';
 import Items from './components/Items/Items';
 import Header from './components/Layout/Header';
@@ -18,6 +19,13 @@ function App() {
 
   return (
     <CartProvider>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>My Game Shop</title>
+        <link rel='canonical' href='http://mysite.com/example' />
+        <meta name='A simple demo site' content='Nested component' />
+      </Helmet>
+
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
